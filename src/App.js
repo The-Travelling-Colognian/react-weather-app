fb184bf297003f537b81ec54906fd731178190d0
Vanessa Schade <82608228+travellingcolognian@users.noreply.github.com>
2021-09-19 15:55:22 +0200

    Updated the Footer link

--- src/App.js
+++ src/App.js
@@ -23,6 +23,15 @@ export default function App() {
             rel="noreferrer"
           >
             open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://competent-almeida-bddaf5.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            hosted on Netlify
           </a>
         </footer>
       </div>
